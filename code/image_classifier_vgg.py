cc77cc9905b7e1c74e47b7f5373e31ac12f5e766
shreyanshu007 <2016csb1060@iitrpr.ac.in>
2019-11-04 23:59:35 +0530

    add subclass text file

--- code/image_classifier_vgg.py
+++ code/image_classifier_vgg.py
@@ -1,5 +1,6 @@
 # Image Classifier
 
+import os
 
 from keras.applications.vgg16 import VGG16
 from keras.utils.vis_utils import plot_model
@@ -9,35 +10,64 @@ from keras.applications.vgg16 import preprocess_input
 from keras.applications.vgg16 import decode_predictions
 
 
-model = VGG16()
-
-# print(model.summary())
-# plot_model(model, to_file='vgg.png')
+# load vgg16 model
+vggmodel = VGG16()
 
 
+def predictLabel(image_path):
 
-# load an image from file
-image = load_img('./image/pig.jpg', target_size=(224, 224))
+	# load an image from file
+	image = load_img(image_path, target_size=(224, 224))
 
-# convert the image pixels to a numpy array
-image = img_to_array(image)
+	# convert the image pixels to a numpy array
+	image = img_to_array(image)
 
-# reshape data for the model
-image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+	# reshape data for the model
+	image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
 
-# prepare the image for the VGG model
-image = preprocess_input(image)
+	# prepare the image for the VGG model
+	image = preprocess_input(image)
 
-# predict the probability across all output classes
-yhat = model.predict(image)
+	# predict the probability across all output classes
+	yhat = vggmodel.predict(image)
 
-# convert the probabilities to class labels
-label = decode_predictions(yhat)
-print(label[0])
+	# convert the probabilities to class labels
+	label = decode_predictions(yhat)
 
-# retrieve the most likely result, e.g. highest probability
-label = label[0][0]
+	# retrieve the most likely result, e.g. highest probability
+	label = label[0][0]
 
-# print the classification
-print('%s (%.2f%%)' % (label[1], label[2]*100))
+	# print the classification
+	# print('%s (%.2f%%)' % (label[1], label[2]*100))
+	return label[1]
 
+
+
+# -------------------------
+
+
+# predictLabel('./../dataset/test/pig.jpg')
+
+slash = '/'
+bird = './../dataset/birds'
+animal = './../dataset/animals'
+
+Tcount = 0
+Fcount = 0
+
+for folder in os.listdir(animal):
+	for file in os.listdir(animal + slash + folder):
+		label = predictLabel(animal + slash + folder + slash + file)
+		if label == folder:
+			Tcount += 1
+		else:
+			Fcount += 1
+			print(label)
+			print(folder)
+			print(file)
+			print('\n\n')
+	# break
+
+
+print(Tcount)
+print(Fcount)
